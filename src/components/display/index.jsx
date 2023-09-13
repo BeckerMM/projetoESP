812e6356f578a9fb172feab17eb9704d580f2540
Matheus Becker <beckerm@weg.net>
2023-09-13 20:44:41 -0300

    projeto finalizado para a apresentação

--- src/components/display/index.jsx
+++ src/components/display/index.jsx
@@ -1,37 +1,57 @@
 
 import React from 'react';
-import { useEffect, useState } from 'react'; 
+import { useEffect, useState } from 'react';
 const Index = (props) => {
 
     const [temperatura, setTemperatura] = useState(0);
-  
-useEffect(() => {
-    setTemperatura(props.temperatura);
-}
-)
+    const [umidade, setUmidade] = useState(0);
+    useEffect(() => {
+
+        setTemperatura(props.temperatura);
+        setUmidade(props.umidade);
+        console.log(umidade)
+    }
+    )
 
     const styleDiv = () => {
-
         if (temperatura >= 27) {
             return "bg-[#eb6e66] ";
-
         } else if (temperatura <= 26 && temperatura >= 19) {
-            return "bg-[#e9d83f] "; 
-
+            return "bg-[#e9d83f] ";
         } else if (temperatura <= 18 && temperatura >= 11) {
-            return "bg-[#7dd85d] " ;
-        }else if(temperatura<=10) {
+            return "bg-[#7dd85d] ";
+        } else if (temperatura <= 10) {
             return "bg-[#3b7ad9] ";
         }
 
     }
 
+    const styleText = () => {
+        if (umidade > 80) {
+            return "bg-blue-700 "
+        } else if (umidade <= 80 && umidade >= 60) {
+            return "bg-blue-500 "
+        } else if (umidade <= 59 && umidade >= 40) {
+            return "bg-blue-100 "
+        } else if (umidade <= 39 && umidade >= 20) {
+            return "bg-red-200 "
+        } else if (umidade <= 19) {
+            return "bg-red-400 "
+        }
+    }
+
+
+
     return (
         <>
-            <div className={styleDiv() + "h-1/6 w-4/6 rounded-lg flex justify-center items-center "}>
-                <p className="text-5xl text-center">{props.temperatura}ºC</p>
-            </div>
-
+            {temperatura != null ?
+                <div className={styleDiv() + "h-1/6 w-4/6 rounded-lg flex justify-center items-center "}>
+                    <p className="text-5xl text-center">{props.temperatura}ºC</p>
+                </div> :
+                <div className={styleText() + "h-1/6 w-4/6 rounded-lg flex justify-center items-center "}>
+                    <p className="text-5xl text-center">{props.umidade}%</p>
+                </div>}
+                
         </>
 
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,14 +4,28 @@ import service from "../service/index";
 import { useEffect, useState } from "react";
 const Index = () => {
 
-   const [temperatura, setTemperatura ] = useState(0);
+    const [temperatura, setTemperatura] = useState(0);
+    const [umidade, setUmidade] = useState(0);
+    const [change, setChange] = useState(0);
 
-   const verifyTemperature = () => {
-    service.get()
-    setTemperatura(service.getValue())
+    const changeDisplay = () => {
+        if (change == 1) {
+            setChange(0);
+        } else {
+            setChange(1);
+        }
+    }
 
-    
-}
+
+    const verifyHumity = () => {
+        service.getHumity();
+        setUmidade(service.getValueHumity());
+    }
+
+    const verifyTemperature = () => {
+        service.get();
+        setTemperatura(service.getValue());
+    }
 
 
 
@@ -20,23 +34,41 @@ const Index = () => {
             <div className=" h-screen w-screen bg-[#f2f2f2] flex justify-center items-center">
                 <div className="h-5/6 w-5/6 flex flex-col items-center gap-5">
 
-                    <div className="text-5xl text-[#454545]">
-                        <p>Temperatura Local:</p>
+                    <div className="h-1/6 w-1/2 flex justify-center items-center">
+                        <button className="h-4/5 w-4/6 bg-[#0d86cb] text-2xl text-white rounded-md" onClick={changeDisplay}>{change == 1 ? "Tmperatura Local" : "Umidade Local"}</button>
                     </div>
-                    
-                    <Display temperatura={temperatura == null ? setTemperatura(0): temperatura } />
-              
 
-                        <button className="text-white h-[8%] w-[20%] bg-[#0d86cb] flex justify-center items-center hover:bg-[#1c94da] rounded-md" onClick={verifyTemperature} >Clique</button>
-                     
+
+                    {change == 1 ?
+                        <>
+                            <div className="text-5xl text-[#454545]">
+                                <p>Temperatura Local:</p>
+                            </div>
+
+                            <Display temperatura={temperatura == null ? setTemperatura(0) : temperatura} />
+
+                            <button className="text-white h-[8%] w-[20%] bg-[#0d86cb] flex justify-center items-center hover:bg-[#1c94da] rounded-md" onClick={() => {
+                                verifyTemperature();
+                            }}>Clique</button></> :
+                        <>
+
+                            <div className="text-5xl text-[#454545]">
+                                <p>Umidade Local:</p>
+                            </div>
+
+                            <Display umidade={umidade == null ? setUmidade(0) : umidade} />
+                            <button className="text-white h-[8%] w-[20%] bg-[#0d86cb] flex justify-center items-center hover:bg-[#1c94da] rounded-md" onClick={() => {
+                                verifyHumity();
+                            }}>Clique</button>
+                        </>}
+
+
                 </div>
 
 
             </div>
 
         </>)
-
-
 }
 
 export default Index;
